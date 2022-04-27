a96ddd8bd4d6a29d90511daafb62c7ee78de06c7
ssankar-23 <70551727+ssankar-23@users.noreply.github.com>
2022-04-27 09:23:44 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit_folium",
+    version="0.6.7",
+    author="Randy Zwitch",
+    author_email="randy@streamlit.io",
+    description="Render Folium objects in Streamlit",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="https://github.com/randyzwitch/streamlit-folium",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "folium>=0.11", "jinja2", "branca"],
+)
